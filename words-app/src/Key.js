588237a84362aea6d48b4a94b3d4e91ed30a585a
Lorenzo <lamu1197@hotmail.com>
2022-07-04 11:47:27 +0200

    ENTER e DELETE

--- words-app/src/Key.js
+++ words-app/src/Key.js
@@ -11,6 +11,18 @@ function Key({keyval,otherkey}) {
         if(keyval === "ENTER"){
             if (currentAttempt.letterPos !== 5) return;
             setCurrentAttempt({attempt:currentAttempt.attempt +1,letterPos:0})
+
+            //funzionalità di enter:non funziona se non si completa la parola
+            //attempt + 1 significa che non rimango sempre nella pos [0,0]
+            //ma va avanti
+        }
+        else if(keyval === "DELETE"){
+            if(currentAttempt.letterPos === 0) return;
+            const newBoard = [...board]
+            newBoard[currentAttempt.attempt][currentAttempt.letterPos -1]= ""; //mi ritorna una stringa vuota
+            setBoard(newBoard)
+            setCurrentAttempt({...currentAttempt,letterPos: currentAttempt.letterPos -1})
+
         }
         else{
             if (currentAttempt.letterPos >4) return;
